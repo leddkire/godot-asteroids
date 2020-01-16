2deb95fb23915f47f8406130db9e9814072a8fd8
leddkire <leddkire@gmail.com>
2020-01-16 22:09:30 +0100

    Implement ship transition around screen
    - It's still buggy, but horizontal screen wrapping seems to work

--- ShipEdgePositionController/ShipEdgePositionController.gd
+++ ShipEdgePositionController/ShipEdgePositionController.gd
@@ -20,3 +20,27 @@ func _on_edge_exited(orientation : int, body : PhysicsBody2D, side_exited: int):
     if body.is_in_group(GroupConstants.DRIFTS):
         if side_exited == Edge_Side.OUTER:
             body.on_outer_edge_exited()
+        if side_exited == Edge_Side.INNER:
+            var ships : Array = self.get_tree().get_nodes_in_group(GroupConstants.SHIP)
+            ships.erase(body)
+            _reposition_ships_around(body,ships)
+
+func _reposition_ships_around(object,objects):
+    var screen_width = ProjectSettings.get_setting("display/window/size/width")
+    var screen_height = ProjectSettings.get_setting("display/window/size/height")
+    assert(objects.size() == 4)
+    var object_without_position = objects.pop_front()
+    object_without_position.position.x = object.position.x + screen_width
+    object_without_position.position.y = object.position.y
+
+    object_without_position = objects.pop_front()
+    object_without_position.position.x = object.position.x - screen_width
+    object_without_position.position.y = object.position.y
+
+    object_without_position = objects.pop_front()
+    object_without_position.position.x = object.position.x
+    object_without_position.position.y = object.position.y + screen_height
+
+    object_without_position = objects.pop_front()
+    object_without_position.position.x = object.position.x
+    object_without_position.position.y = object.position.y - screen_height
