35c3f8e79129a547316e3d58ccfa31e344d645d2
leddkire <leddkire@gmail.com>
2020-02-27 22:08:35 +0100

    Refactor movement and pellet-angle calculations

--- ship/ship.gd
+++ ship/ship.gd
@@ -7,7 +7,6 @@ onready var current_rotation_direction : int = 0
 onready var rotation_speed : float = 2.5
 onready var friction : float = 0.99
 onready var shooting_angle : float = 0
-var direction_vector : Vector2
 
 signal entity_shooting
 
@@ -19,19 +18,22 @@ func _ready():
 
 func _input(input_event : InputEvent):
     if input_event.is_action("shoot_projectile"):
-        var pellet = self.spawn_pellet_projectile(get_projectile_spawn_position(), direction_vector.angle())
+        var pellet_shooting_angle = Vector2(1,1).rotated(self.rotation).angle()
+        var pellet = spawn_pellet_projectile(get_projectile_spawn_position(), pellet_shooting_angle)
         self.get_parent().add_child(pellet)
 
 func _physics_process(delta):
-    current_rotation_direction = calculate_rotation_direction_from_input()
-    rotation += current_rotation_direction * rotation_speed * delta
-    direction_vector = Vector2(1,1).rotated(rotation)
-    var scoped_rotation = rotation
-    if Input.is_action_pressed("move_up") :
-        current_velocity_vector += Vector2(sin(scoped_rotation),-cos(scoped_rotation)) * thrust * delta
-    current_velocity_vector = current_velocity_vector * friction
+    self.rotation += calculate_rotation_direction_from_input() * self.rotation_speed * delta
+    self.current_velocity_vector = calculate_movement_vector_based_on_input(self.current_velocity_vector, self.rotation, delta)
     move_and_collide(self.current_velocity_vector)
 
+func calculate_movement_vector_based_on_input(velocity_vector, current_rotation, delta):
+    if Input.is_action_pressed("move_up") :
+        velocity_vector += calculate_velocity(current_rotation) * delta
+    return velocity_vector * self.friction
+
+func calculate_velocity(current_rotation):
+    return Vector2(sin(current_rotation),-cos(current_rotation)) * self.thrust
 
 func calculate_rotation_direction_from_input():
     if Input.is_action_pressed("move_left"):
@@ -39,15 +41,6 @@ func calculate_rotation_direction_from_input():
     if Input.is_action_pressed("move_right") :
         return 1
     return 0
-
-func calculate_velocity_from_input(current_velocity,acceleration,max_speed):
-    if Input.is_action_pressed("move_up") :
-        var velocity : float = current_velocity +  acceleration
-        if(velocity > max_speed):
-            velocity = max_speed
-        return velocity
-    else:
-        return current_velocity
 
 func _on_collision_area_entered(area):
     print("Entered: " + area.name)
