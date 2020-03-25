10a79d5c50532abece5ecd6720a0a5940ba74854
leddkire <leddkire@gmail.com>
2020-03-25 21:38:46 +0100

    Add descriptive comments

--- asteroids/Asteroid.gd
+++ asteroids/Asteroid.gd
@@ -8,10 +8,12 @@ func _ready():
 
 func set_new_position(pos: Vector2):
     var current_velocity = self.linear_velocity
+    # The empty _integrate_forces method ensures that the body doesn't move
     self.custom_integrator = true
     self.global_transform.origin = pos
     self.linear_velocity = current_velocity
-    self.custom_integrator = false
+    self.custom_integrator = false # Restore physics processing on body
 
 func _integrate_forces(state):
+    # Empty because we want the body to stop moving.
     pass
