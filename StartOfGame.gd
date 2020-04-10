3d72eff9dc87341f1132cc689929093c680e2c3c
leddkire <leddkire@gmail.com>
2020-04-10 18:55:37 +0200

    Rename main project scene

--- StartOfGame.gd
+++ StartOfGame.gd
@@ -2,12 +2,14 @@
 
 var shipScene = preload("res://ship/ship.tscn")
 const sides_and_corners_of_screen = 8
+export (int) var asteroids_to_spawn = 5
 
 func _ready():
     var entity_screen_wrap_rule : EntityScreenWrapRule = load("res://rules/screen_wrap/EntityScreenWrapRule.gd").new()
     add_child(create_ship_position_controller(entity_screen_wrap_rule))
     spawn_ship(entity_screen_wrap_rule)
-    spawn_asteroids(entity_screen_wrap_rule)
+    for i in asteroids_to_spawn:
+        spawn_asteroids(entity_screen_wrap_rule)
 
 func create_ship_position_controller(wrap_rule: EntityScreenWrapRule):
     var edges : Array = get_tree().get_nodes_in_group("edges")
@@ -17,25 +19,35 @@ func create_ship_position_controller(wrap_rule: EntityScreenWrapRule):
 
 
 func spawn_ship(wrap_rule : EntityScreenWrapRule):
-    var ships : Array = get_tree().get_nodes_in_group("ships")
-    assert(ships.size() == 1)
-    var central_ship = ships.front()
-    central_ship.screen_id = EntityCensus.issue_new_id()
-    EntityCensus.add_entity_to_census(central_ship)
-    ships = generate_screen_wrap_objects(shipScene, sides_and_corners_of_screen, central_ship.screen_id)
+    var central_ship = get_existing_ship()
+    add_to_census(central_ship)
+    var ships = generate_screen_wrap_objects(shipScene, sides_and_corners_of_screen, central_ship.screen_id)
     wrap_rule.reposition_around(central_ship,ships)
 
+func get_existing_ship():
+    var ships : Array = get_tree().get_nodes_in_group("ships")
+    assert(ships.size() == 1)
+    return ships.front()
+
+func add_to_census(entity):
+    entity.screen_id = EntityCensus.issue_new_id()
+    EntityCensus.add_entity_to_census(entity)
+
+
+func calculate_position():
+    var upper_y_limit = ProjectSettings.get_setting("display/window/size/height")
+    var upper_x_limit = ProjectSettings.get_setting("display/window/size/width")
+    return Vector2(randi() % upper_x_limit, randi() % upper_y_limit)
 
 func spawn_asteroids(entity_screen_wrap_rule):
-    var asteroid_scene = load("res://asteroids/Asteroid.tscn")
-    var center_asteroid = asteroid_scene.instance()
-    center_asteroid.position = Vector2(100,100) # Position should be random
-    add_child(center_asteroid)
-    center_asteroid.size = "L"
-    center_asteroid.screen_id = EntityCensus.issue_new_id()
-    EntityCensus.add_entity_to_census(center_asteroid)
-    var surrounding_asteroids = generate_asteroid_screen_wrap_objects(asteroid_scene, 8, center_asteroid.screen_id)
-    entity_screen_wrap_rule.reposition_around(center_asteroid, surrounding_asteroids)
+    var asteroid_spawning_rule = load("res://rules/position/AsteroidSpawningRule.gd").new(entity_screen_wrap_rule)
+    var asteroids = asteroid_spawning_rule.spawn_asteroids()
+    var screen_id = EntityCensus.issue_new_id()
+    for asteroid in asteroids:
+        asteroid.screen_id = screen_id
+        add_child(asteroid)
+        EntityCensus.add_entity_to_census(asteroid)
+
 
 func generate_asteroid_screen_wrap_objects(scene, amount_to_instance, screen_id):
     var objects = []
