687c4f573758acd5458aa446bce5cc495c33d130
leddkire <leddkire@gmail.com>
2020-04-15 23:01:32 +0200

    Refactor ScreenWrapController

--- ScreenWrapController/ScreenWrapController.gd
+++ ScreenWrapController/ScreenWrapController.gd
@@ -16,14 +16,18 @@ func _init(edges, entity_screen_wrap_rule, entity_screen_census: EntityCensus):
         edge.connect("edge_exited",self,"_on_edge_exited")
 
 func _on_edge_exited(orientation : int, body, side_exited: int):
-    if is_instance_valid(body) :
-        if body.is_in_group(GroupConstants.DRIFTS):
-            if (side_exited == Edge_Side.INNER and is_inside_screen(body)) :
-                var entities = entity_screen_census.get_in_census(body.screen_id)
-                assert(entities.size() == 9)
-                if(entities != null and !entities.empty()):
-                    entities.erase(body)
-                    entity_screen_wrap_rule.call_deferred("reposition_around", body, entities)
+    if is_valid_driftable_body_that_exited_inner_edge(body, side_exited):
+        var entities = entity_screen_census.get_in_census(body.screen_id)
+        assert(entities.size() == 9)
+        if(entities != null and !entities.empty()):
+            entities.erase(body)
+            entity_screen_wrap_rule.call_deferred("reposition_around", body, entities)
+
+func is_valid_driftable_body_that_exited_inner_edge(body, side_exited):
+    return is_instance_valid(body) and body.is_in_group(GroupConstants.DRIFTS) and exited_inner_edge(body, side_exited)
+
+func exited_inner_edge(body, side_exited):
+    return side_exited == Edge_Side.INNER and is_inside_screen(body)
 
 func is_inside_screen(body):
     var screen_width = ProjectSettings.get_setting("display/window/size/width")
